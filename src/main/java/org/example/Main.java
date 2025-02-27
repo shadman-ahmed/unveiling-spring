a8cbee590f17d2148fd53456691fba29252b0ab4
Shadman Ahmed <shadman.siam.ahmed@gmail.com>
2025-02-27 10:36:35 +0600

    Add spring-context
    - Added spring-context to show case how xml based config file is
      used to manage DI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,18 @@
 package org.example;
 
 import org.example.service.QuoteService;
-import org.example.service.impl.QuoteServiceImpl;
-import org.example.service.impl.RandomNumberGeneratorImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
-        QuoteService quoteService = new QuoteServiceImpl(new RandomNumberGeneratorImpl());
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("context.xml");
+
+        QuoteService quoteService = applicationContext.getBean("quoteService", QuoteService.class);
+
         String quoteOfTheDay = quoteService.getQuote();
+
         System.out.println("\n\n===> Quote of day: " + quoteOfTheDay + " <===");
     }
 }
